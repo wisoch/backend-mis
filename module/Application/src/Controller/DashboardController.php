43b85ce8955353c44ac0f1d5a1948bf45d6eaf28
王超 <chao.wang@hqq.vip>
2017-11-15 19:20:21 +0800

    backend

--- module/Application/src/Controller/DashboardController.php
+++ module/Application/src/Controller/DashboardController.php
@@ -13,33 +13,5 @@ class DashboardController extends AbstractActionController
      */
     public function homeAction()
     {
-        $return = ['err' => '0', 'msg' => '', 'ret' => []];
-
-        $request = $this->getRequest();
-        $content = $request->getContent();
-
-        $context = json_decode($content, true);
-
-        $application = $this->getEvent()->getApplication();
-        $services    = $application->getServiceManager();
-
-        $config = $services->get('Config');
-
-        $interface  = $config['interfaces'][$context['method']];
-
-        $controller = $interface['controller'];
-        $action     = $interface['action'];
-
-        $params = [];
-        $params['action'] = $action;
-        $params += $context['params'];
-
-        $result = $this->forward()->dispatch($controller, $params);
-        $result = $result->getVariables();
-        if ($result) {
-            $return['ret'] = $result;
-        }
-
-        return new JsonModel($return);
     }
 }
